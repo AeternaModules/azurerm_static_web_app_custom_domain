981178f3ec3ca42c329f46e979c7f6e7ed2766a3
Daniel Allen <dan@aeternagroup.net>
2026-01-11 19:27:45 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "static_web_app_custom_domains" {
+  description = "All static_web_app_custom_domain resources"
+  value       = azurerm_static_web_app_custom_domain.static_web_app_custom_domains
+}
+
