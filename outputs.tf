9addebe328ba3728296a34e1db34596e9d14f0ec
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:51:15 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,22 +1,22 @@
 output "static_web_app_custom_domains_id" {
   description = "Map of id values across all static_web_app_custom_domains, keyed the same as var.static_web_app_custom_domains"
-  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.id }
+  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "static_web_app_custom_domains_domain_name" {
   description = "Map of domain_name values across all static_web_app_custom_domains, keyed the same as var.static_web_app_custom_domains"
-  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.domain_name }
+  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.domain_name if v.domain_name != null && length(v.domain_name) > 0 }
 }
 output "static_web_app_custom_domains_static_web_app_id" {
   description = "Map of static_web_app_id values across all static_web_app_custom_domains, keyed the same as var.static_web_app_custom_domains"
-  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.static_web_app_id }
+  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.static_web_app_id if v.static_web_app_id != null && length(v.static_web_app_id) > 0 }
 }
 output "static_web_app_custom_domains_validation_token" {
   description = "Map of validation_token values across all static_web_app_custom_domains, keyed the same as var.static_web_app_custom_domains"
-  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.validation_token }
+  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.validation_token if v.validation_token != null && length(v.validation_token) > 0 }
   sensitive   = true
 }
 output "static_web_app_custom_domains_validation_type" {
   description = "Map of validation_type values across all static_web_app_custom_domains, keyed the same as var.static_web_app_custom_domains"
-  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.validation_type }
+  value       = { for k, v in azurerm_static_web_app_custom_domain.static_web_app_custom_domains : k => v.validation_type if v.validation_type != null && length(v.validation_type) > 0 }
 }
 
